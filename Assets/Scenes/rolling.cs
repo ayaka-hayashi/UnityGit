7f7e2cdbf8482ad4f6a0821a2b601a59f14a614c
HAYASHI <ayaka.hayashi13@gmail.com>
2025-09-24 18:01:37 +0900

    object追加

--- Assets/Scenes/rolling.cs
+++ Assets/Scenes/rolling.cs
@@ -5,6 +5,9 @@ public class rolling : MonoBehaviour
     public float rollX;
     public float rollY;
     public float rollZ;
+    public float posX;
+    public float posY;
+    public float posZ;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +19,7 @@ public class rolling : MonoBehaviour
     void Update()
     {
         transform.Rotate(rollX, rollY, rollZ);
+        //transform.position(posX, posY, posZ);
+         transform.Translate(Vector3.right * 0.1f * Time.deltaTime);
     }
 }
